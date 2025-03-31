5403fbf707061b37eafd1f1cd951a96f7a9770f7
Sirojiddin Fazliddin <sirojiddin03101992@gmail.com>
2025-03-31 18:13:58 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = "aviatop2025Token"
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        mode = request.args.get('hub.mode')
+        token = request.args.get('hub.verify_token')
+        challenge = request.args.get('hub.challenge')
+        if mode == 'subscribe' and token == VERIFY_TOKEN:
+            print("Webhook tasdiqlandi!")
+            return challenge, 200
+        else:
+            return 'Verification failed', 403
+
+    elif request.method == 'POST':
+        data = request.json
+        print("🟢 Yangi Instagram xabar:", data)
+        return 'OK', 200
+
+from waitress import serve
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=5000)
